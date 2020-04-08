6e72b4eb4496fbdf667ff4a985004a34ccea52a1
AdeDom <pathiphonyw@gmail.com>
2020-04-08 11:23:49 +0700

    check context on fragment and delete arrow back activity main also set color option toolbar white

--- app/src/main/java/com/chococard/carwash/ui/main/MainActivity.kt
+++ app/src/main/java/com/chococard/carwash/ui/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.chococard.carwash.ui.main
 
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -8,6 +10,7 @@ import com.chococard.carwash.R
 import com.chococard.carwash.ui.main.history.HistoryFragment
 import com.chococard.carwash.ui.main.map.MapFragment
 import com.chococard.carwash.ui.main.wallet.WalletFragment
+import com.chococard.carwash.util.extension.toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,6 +19,13 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+        toolbar.overflowIcon?.setColorFilter(
+            resources.getColor(android.R.color.white),
+            PorterDuff.Mode.SRC_ATOP
+        )
 
         bottom_navigation.setOnNavigationItemSelectedListener(this)
         if (savedInstanceState == null) replaceFragment(MapFragment())
@@ -34,5 +44,20 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         supportFragmentManager.beginTransaction()
             .replace(R.id.frame_layout, fragment)
             .commit()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_option, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.option_change_profile -> toast("option_change_profile")
+            R.id.option_change_password -> toast("option_change_password")
+            R.id.option_contact_admin -> toast("option_contact_admin")
+            R.id.option_logout -> toast("option_logout")
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
--- app/src/main/java/com/chococard/carwash/ui/main/history/HistoryFragment.kt
+++ app/src/main/java/com/chococard/carwash/ui/main/history/HistoryFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.chococard.carwash.R
+import com.chococard.carwash.util.extension.toast
+import kotlinx.android.synthetic.main.fragment_history.*
 
 class HistoryFragment : Fragment() {
 
@@ -14,5 +16,12 @@ class HistoryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_history, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        fab.setOnClickListener {
+            context?.toast("hello")
+        }
+    }
 
 }
