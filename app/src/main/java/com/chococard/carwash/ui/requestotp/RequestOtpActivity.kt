d05871b909c5bd1045537adecbdef61ae50f0f27
AdeDom <pathiphonyw@gmail.com>
2020-05-25 16:43:28 +0700

    create RequestOtpActivity for test shadow activity_request_otp

---
+++ app/src/main/java/com/chococard/carwash/ui/requestotp/RequestOtpActivity.kt
@@ -0,0 +1,14 @@
+package com.chococard.carwash.ui.requestotp
+
+import android.os.Bundle
+import com.chococard.carwash.R
+import com.chococard.carwash.ui.base.BaseActivity
+
+class RequestOtpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_request_otp)
+    }
+
+}
