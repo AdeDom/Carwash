eb423ac1a4d769c5683219419e59f4d4b62b30d0
AdeDom <pathiphonyw@gmail.com>
2020-05-25 17:09:14 +0700

    create VerifyPhoneActivity for test shadow activity_verify_phone

---
+++ app/src/main/java/com/chococard/carwash/ui/verifyphone/VerifyPhoneActivity.kt
@@ -0,0 +1,14 @@
+package com.chococard.carwash.ui.verifyphone
+
+import android.os.Bundle
+import com.chococard.carwash.R
+import com.chococard.carwash.ui.base.BaseActivity
+
+class VerifyPhoneActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify_phone)
+    }
+
+}
