2785c5ccc1ae69db1d3174284856f8b46e457845
AdeDom <pathiphonyw@gmail.com>
2020-06-05 10:54:42 +0700

    implement map on NavigationActivity.kt and move navigation button

--- app/src/main/java/com/chococard/carwash/ui/navigation/NavigationActivity.kt
+++ app/src/main/java/com/chococard/carwash/ui/navigation/NavigationActivity.kt
@@ -2,33 +2,57 @@ package com.chococard.carwash.ui.navigation
 
 import android.location.Location
 import android.os.Bundle
-import android.util.Log
+import android.view.WindowManager
 import androidx.lifecycle.Observer
 import com.chococard.carwash.R
 import com.chococard.carwash.ui.base.BaseLocationActivity
 import com.chococard.carwash.viewmodel.NavigationViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_navigation.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class NavigationActivity : BaseLocationActivity() {
+class NavigationActivity : BaseLocationActivity(), OnMapReadyCallback {
 
     val viewModel: NavigationViewModel by viewModel()
+    private var mGoogleMap: GoogleMap? = null
+    private var mIsFlagMoveCamera: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
 
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        setToolbar(toolbar)
+
+        val mapFragment = fragmentManager.findFragmentById(R.id.map_fragment) as MapFragment
+        mapFragment.getMapAsync(this@NavigationActivity)
+
         viewModel.getJob.observe(this, Observer { job ->
             if (job == null) return@Observer
-            Log.d(TAG, "onCreate: $job")
         })
     }
 
-    override fun onLocationChanged(location: Location?) {
-        Log.d(TAG, "onLocationChanged: ${location?.latitude}, ${location?.longitude}")
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mGoogleMap = googleMap
+        mGoogleMap?.isMyLocationEnabled = true
+        mGoogleMap?.setMinZoomPreference(12F)
+        mGoogleMap?.setMaxZoomPreference(16F)
     }
 
-    companion object {
-        private const val TAG = "NavigationActivity"
+    override fun onLocationChanged(location: Location?) {
+        if (location == null) return
+        val latLng = LatLng(location.latitude, location.longitude)
+
+        if (mIsFlagMoveCamera) {
+            mIsFlagMoveCamera = false
+            val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 14F)
+            mGoogleMap?.moveCamera(cameraUpdate)
+        }
     }
 
 }
