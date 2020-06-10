3d54b663f3b879bb3cbddb4b74d6f2076c978cf6
AdeDom <pathiphonyw@gmail.com>
2020-06-10 16:16:32 +0700

    change UI history & history detail

--- app/src/main/java/com/chococard/carwash/util/FlagConstant.kt
+++ app/src/main/java/com/chococard/carwash/util/FlagConstant.kt
@@ -5,8 +5,6 @@ object FlagConstant {
     const val EMPLOYEE = 3 // employee sign up
     const val JOB_REJECT = 0 // reject from job dialog cancel or timeout
     const val JOB_RECEIVE = 1 // receive from job dialog confirm
-    const val PAYMENT_CONFIRM = 0 // payment dialog confirm button
-    const val PAYMENT_REPORT = 1 // payment dialog report button
     const val LOGS_STATUS_INACTIVE = 0 // set user logs inactive on home screen from main activity
     const val LOGS_STATUS_ACTIVE = 1 // set user logs active on open application
     const val SWITCH_OFF = 0 // switch system for receive job off
