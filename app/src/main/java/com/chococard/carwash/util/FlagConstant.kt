5249085d1724638e2cdc6551d18daec50441f172
AdeDom <pathiphonyw@gmail.com>
2020-06-11 15:15:10 +0700

    delete file and values at not using

--- app/src/main/java/com/chococard/carwash/util/FlagConstant.kt
+++ app/src/main/java/com/chococard/carwash/util/FlagConstant.kt
@@ -9,7 +9,6 @@ object FlagConstant {
     const val LOGS_STATUS_ACTIVE = 1 // set user logs active on open application
     const val SWITCH_OFF = 0 // switch system for receive job off
     const val SWITCH_ON = 1 // switch system for receive job on
-    const val STATUS_SERVICE_NORMAL = 0
     const val STATUS_SERVICE_FRONT = 1
     const val STATUS_SERVICE_BACK = 2
     const val STATUS_SERVICE_LEFT = 3
