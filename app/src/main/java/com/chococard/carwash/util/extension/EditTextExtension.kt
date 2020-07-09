8a1678b5e449cf1533be60b4aedc6d5d450f5318
AdeDom <pathiphonyw@gmail.com>
2020-07-09 14:45:57 +0700

    delete code extension isEmpty, isMinLength, isEqualLength, isMatched, isVerifyIdentityCard, isVerifyPhone to validate on view model

--- app/src/main/java/com/chococard/carwash/util/extension/EditTextExtension.kt
+++ app/src/main/java/com/chococard/carwash/util/extension/EditTextExtension.kt
@@ -9,61 +9,6 @@ import com.chococard.carwash.util.FlagConstant
 
 fun EditText.getContents() = this.text.toString().trim()
 
-// todo delete edit text set error
-fun EditText.isEmpty(error: String = ""): Boolean {
-    if (this.getContents().isEmpty()) {
-        this.requestFocus()
-        this.error = error
-        return true
-    }
-    return false
-}
-
-fun EditText.isMinLength(length: Int, error: String = ""): Boolean {
-    if (this.getContents().length < length) {
-        this.requestFocus()
-        this.error = error
-        return true
-    }
-    return false
-}
-
-fun EditText.isEqualLength(length: Int, error: String = ""): Boolean {
-    if (this.getContents().length != length) {
-        this.requestFocus()
-        this.error = error
-        return true
-    }
-    return false
-}
-
-fun EditText.isMatched(editText: EditText, error: String = ""): Boolean {
-    if (this.getContents() != editText.getContents()) {
-        editText.requestFocus()
-        editText.error = error
-        return true
-    }
-    return false
-}
-
-fun EditText.isVerifyIdentityCard(error: String = ""): Boolean {
-    if (this.getContents().isVerifyIdentityCard()) {
-        this.requestFocus()
-        this.error = error
-        return true
-    }
-    return false
-}
-
-fun EditText.isVerifyPhone(error: String = ""): Boolean {
-    if (this.getContents().isVerifyPhone()) {
-        this.requestFocus()
-        this.error = error
-        return true
-    }
-    return false
-}
-
 fun EditText.onTextChanged(text: (String) -> Unit) {
     this.addTextChangedListener(object : TextWatcher {
         override fun afterTextChanged(s: Editable?) {}
