fcde6ada209a4139857cd273c4c633037a1bd29b
AdeDom <pathiphonyw@gmail.com>
2020-07-29 14:25:25 +0700

    delete old class SafeApiRequest.kt, HeaderAppService.kt, HeaderRepository.kt

--- app/src/main/java/com/chococard/carwash/di/AppModule.kt
+++ app/src/main/java/com/chococard/carwash/di/AppModule.kt
@@ -28,9 +28,7 @@ val appModule = module {
 
     //header connection
     single { NetworkHeaderInterceptor(get(), get()) }
-    single { HeaderAppService.invoke(get()) }
     single { HeaderAppServiceV2.invoke(get()) }
-    single<HeaderRepository> { HeaderRepositoryImpl(get(), get(), get()) }
     single<HeaderRepositoryV2> { HeaderRepositoryV2Impl(get(), get(), get()) }
     viewModel { AddWalletViewModel(get()) }
     viewModel { ChangePasswordViewModel(get()) }
