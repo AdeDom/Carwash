b0a64ffbb82e7da89e23f200c66e5ffbcfe0bbc6
AdeDom <pathiphonyw@gmail.com>
2020-09-15 15:32:23 +0700

    Change base url from dev chococrm to sn carwash and add connect for time out

--- app/src/main/java/com/chococard/carwash/data/networks/RetrofitClient.kt
+++ app/src/main/java/com/chococard/carwash/data/networks/RetrofitClient.kt
@@ -4,15 +4,19 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
-    const val BASE_URL = "https://dev.chococrm.com:10004/"
+    const val BASE_URL = "https://sncarwash.azurewebsites.net/"
 
     fun instant(interceptor: Interceptor): Retrofit {
-        val okHttpClient = OkHttpClient.Builder()
-            .addInterceptor(interceptor)
-            .build()
+        val okHttpClient = OkHttpClient.Builder().apply {
+            addInterceptor(interceptor)
+            connectTimeout(60, TimeUnit.SECONDS)
+            writeTimeout(60, TimeUnit.SECONDS)
+            readTimeout(60, TimeUnit.SECONDS)
+        }.build()
 
         return Retrofit.Builder().apply {
             baseUrl(BASE_URL)
